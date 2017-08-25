e549bf482971ffa604112b4e685b0daa020b70d1
Samuel M.H <samuel.munoz@beeva.com>
2017-08-25 12:05:11 +0200

    More main programs

---
+++ code/beebot-demo_day.py
@@ -0,0 +1,37 @@
+
+import time
+
+from body.body import Body
+
+
+
+b = Body()
+time.sleep(5)
+
+b.resources['elbow_right'].move(90)
+b.resources['elbow_left'].move(90)
+time.sleep(1)
+b.resources['shoulder_right'].move(0)
+b.resources['shoulder_left'].move(0)
+time.sleep(.5)
+b.resources['elbow_right'].move(180)
+b.resources['elbow_left'].move(180)
+time.sleep(3)
+b.resources['head_pan'].move(50)
+time.sleep(.5)
+b.resources['head_pan'].move(130)
+time.sleep(.5)
+b.resources['head_pan'].move(90)
+time.sleep(.5)
+b.resources['head_tilt'].move(130)
+time.sleep(.5)
+b.resources['head_tilt'].move(50)
+time.sleep(.5)
+b.resources['head_tilt'].move(90)
+
+b.stop()
+
+b = Body()
+b.stop()
+
+# b.resources['platform'].forward(0.3)
