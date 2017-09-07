b89c158ee660ac24ad94a05c9db676caf7aff596
Samuel M.H <samuel.munoz@beeva.com>
2017-09-07 10:24:35 +0200

    keyboard controller with delay

--- beebot/beebot-local-client.py
+++ beebot/beebot-local-client.py
@@ -11,7 +11,7 @@ try:
     body = Body()
     print("I am alive!\nPress Ctrl-D to exit:")
     client = Local(body)
-    Keyboard(client).loop()
+    Keyboard(client, 0).loop()
 except Exception as e:
     print(e)
 finally:
